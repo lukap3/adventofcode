9a818e3ee5c4d1ad0dc3697e66370a6b6ea6e24d
Luka Perović <32458554+lukap3@users.noreply.github.com>
2023-12-03 09:07:26 +0100

    Add common dependency, fix scripts/template

--- scripts/new.sh
+++ scripts/new.sh
@@ -4,6 +4,7 @@ directory="$YEAR/days"
 day_numbers=$(find "$directory" -type f -name "day*.py" -exec basename {} \; | grep -o -E '[0-9]+' | sort -n)
 
 max_day=$(echo "$day_numbers" | tail -n 1)
+max_day=$((10#$max_day))
 next=$((max_day+1))
 next=$(printf "%02d" $next)
 
@@ -16,7 +17,7 @@ mkdir $YEAR/data/day$next
 touch $YEAR/data/day$next/data.txt
 touch $YEAR/data/day$next/example.txt
 cp template $YEAR/days/day$next.py
-sed -i '' "s/dayX/day$next/" $YEAR/days/day$next.py
+sed -i '' "s/dayX/$YEAR\/data\/day$next/" $YEAR/days/day$next.py
 
 if python fetch.py ${YEAR} $next; then
   echo "Files generated"
